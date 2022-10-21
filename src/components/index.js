7bfb8d644f82e4a8b07971424dd54a884418942c
armod <armods@gmail.com>
2022-10-21 14:20:50 +0200

    search git users

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Info from './Info'
+import Repos from './Repos'
+import User from './Repos'
+import Search from './Search'
+import Navbar from './Navbar'
+
+export { Info, Repos, User, Search, Navbar }
