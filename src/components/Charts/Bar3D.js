543f62f12de42d3c051e31c287d0eb78bbddd00f
armod <armods@gmail.com>
2022-10-21 21:49:48 +0200

    github users search

---
+++ src/components/Charts/Bar3D.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Bar3d = () => {
+  return <div>Bar3d</div>
+}
+
+export default Bar3d
--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,7 +1,68 @@
 import React from 'react'
+import styled from 'styled-components'
+import { GithubContext } from '../context/context'
+import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts'
 
 const Repos = () => {
-  return <div>Repos</div>
+  const { repos } = React.useContext(GithubContext)
+
+  let languages = repos.reduce((total, item) => {
+    const { language } = item
+    if (!language) return total
+    // console.log(language)
+    if (!total[language]) {
+      total[language] = { label: language, value: 1 }
+    } else {
+      total[language] = { ...total[language], value: total[language].value + 1 }
+    }
+    return total
+  }, {})
+  console.log(languages)
+  const chartData = [
+    {
+      label: 'HTML',
+      value: '13',
+    },
+    {
+      label: 'CSS',
+      value: '20',
+    },
+    {
+      label: 'JavaScript',
+      value: '18',
+    },
+  ]
+  return (
+    <section className='section'>
+      <Wrapper className='section-center'>
+        {/* <ExampleChart data={chartData} /> */}
+        <Pie3D data={chartData} />
+      </Wrapper>
+    </section>
+  )
 }
+
+const Wrapper = styled.div`
+  display: grid;
+  justify-items: center;
+  gap: 2rem;
+  @media (min-width: 800px) {
+    grid-template-columns: 1fr 1fr;
+  }
+  @media (min-width: 1200px) {
+    grid-template-columns: 2fr 3fr;
+  }
+
+  div {
+    width: 100% !important;
+  }
+  .fusioncharts-container {
+    width: 100% !important;
+  }
+  svg {
+    width: 100% !important;
+    border-radius: var(--radius) !important;
+  }
+`
 
 export default Repos
