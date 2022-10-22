fbeab6987c7d797c99d8f22b6a159578d71f7f93
armod <armods@gmail.com>
2022-10-22 20:50:41 +0200

    github users search

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css'
 import App from './App'
 import { GithubProvider } from './context/context'
 import { Auth0Provider } from '@auth0/auth0-react'
+// dev-6x1vjgpryfcbbxgx.us.auth0.com
+// yjykSOfJmt4nRdg9XtLuRDieJ2wgJrrK
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
