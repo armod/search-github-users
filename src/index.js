4e5f6b9652091e0e3bf8e951476632f70e076865
armod <armods@gmail.com>
2022-10-22 22:44:32 +0200

    github users search

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { Auth0Provider } from '@auth0/auth0-react'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Auth0Provider domain='dev-6x1vjgpryfcbbxgx.us.auth0.com' clientId='yjykSOfJmt4nRdg9XtLuRDieJ2wgJrrK' redirectUri={window.location.origin}>
+    <Auth0Provider domain='dev-6x1vjgpryfcbbxgx.us.auth0.com' clientId='yjykSOfJmt4nRdg9XtLuRDieJ2wgJrrK' redirectUri={window.location.origin} cacheLocation='localstorage'>
       <GithubProvider>
         <App />
       </GithubProvider>
